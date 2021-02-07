a901711fae406194f6164fe264b81fd5f98117a9
jeffjl74 <jeffjl.kde@outlook.com>
2021-02-06 22:42:53 -0600

    make sure it is on-screen

--- Overlay/DeathLog.cs
+++ Overlay/DeathLog.cs
@@ -42,7 +42,27 @@ namespace Overlay
             Point pt = parentRect.Location;
             pt.X += parentRect.Width + 5;
             Location = pt;
+            EnsureVisible(this);
             this.TopMost = true;
+        }
+
+        /// <summary>
+        /// Adjusts the form, if necessary, so that it is completely on-screen
+        /// </summary>
+        /// <param name="ctrl"></param>
+        private void EnsureVisible(Control ctrl)
+        {
+            Rectangle ctrlRect = ctrl.DisplayRectangle; //The dimensions of the ctrl
+            ctrlRect.Y = ctrl.Top; //Add in the real Top and Left Vals
+            ctrlRect.X = ctrl.Left;
+            Rectangle screenRect = Screen.GetWorkingArea(ctrl); //The Working Area for the screen showing most of the Ctrl
+
+            //Now tweak the ctrl's Top and Left until it's fully visible. 
+            ctrl.Left += Math.Min(0, screenRect.Left + screenRect.Width - ctrl.Left - ctrl.Width);
+            ctrl.Left -= Math.Min(0, ctrl.Left - screenRect.Left);
+            ctrl.Top += Math.Min(0, screenRect.Top + screenRect.Height - ctrl.Top - ctrl.Height);
+            ctrl.Top -= Math.Min(0, ctrl.Top - screenRect.Top);
+
         }
 
         /// <summary>
